40436bcc01a8cf0002933fb8879667fd9b807bd7
Namya Aankur Gupta <65950853+Namyagupta@users.noreply.github.com>
2022-10-04 00:05:18 +0530

    Create sales_analysis3.sql

---
+++ sales_analysis3.sql
@@ -0,0 +1,37 @@
+/*
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table has no primary key, it can have repeated rows.
+product_id is a foreign key to the Product table.
+Each row of this table contains some information about one sale.
+ 
+
+Write an SQL query that reports the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
+*/
+
+SELECT p.product_id, p.product_name 
+FROM Product p JOIN Sales s 
+ON p.product_id = s.product_id
+GROUP BY p.product_id
+HAVING MIN(sale_date)>='2019-01-01' AND MAX(sale_date)<='2019-03-31';
