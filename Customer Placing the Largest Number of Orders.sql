069bf0fb52e5f4024efb7dc1aec033db7466069f
Namya Aankur Gupta <65950853+Namyagupta@users.noreply.github.com>
2022-10-03 20:09:24 +0530

    max_orders

---
+++ Customer Placing the Largest Number of Orders.sql
@@ -0,0 +1,6 @@
+--Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+
+SELECT customer_number FROM orders
+GROUP BY customer_number
+ORDER BY count(order_number) DESC
+LIMIT 1;
