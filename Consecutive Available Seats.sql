44f21c959deb7bc527b43a6aa572dc4e24e55ee2
Namya Aankur Gupta <65950853+Namyagupta@users.noreply.github.com>
2022-10-05 19:43:23 +0530

    Update Consecutive Available Seats.sql

--- Consecutive Available Seats.sql
+++ Consecutive Available Seats.sql
@@ -1,6 +1,22 @@
 --Consecutive Available Seats
--- Write an SQL query to report all the consecutive available seats in the cinema. Return the result table ordered by seat_id in ascending order.
+/*
+Table: Cinema
 
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| seat_id     | int  |
+| free        | bool |
++-------------+------+
+seat_id is an auto-increment primary key column for this table.
+Each row of this table indicates whether the ith seat is free or not. 1 means free while 0 means occupied.
+ 
+
+Write an SQL query to report all the consecutive available seats in the cinema.
+
+Return the result table ordered by seat_id in ascending order.
+
+*/
 
 SELECT DISTINCT c1.seat_id
 FROM cinema c1 JOIN cinema c2
