c79286102d1d4fdf18adc3fa256aaf89b0609036
Namya Aankur Gupta <65950853+Namyagupta@users.noreply.github.com>
2022-10-05 19:14:53 +0530

    Create game_analysis2.sql

---
+++ game_analysis2.sql
@@ -0,0 +1,22 @@
+/*
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+ 
+
+Write an SQL query to report the device that is first logged in for each player.
+*/
+
+SELECT player_id, device_id
+FROM Activity WHERE (player_id, event_date) IN 
+(select player_id, MIN(event_date) FROM Activity GROUP BY player_id)
