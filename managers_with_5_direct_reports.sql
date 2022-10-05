f6b362a21f31602090a0d72d494e50641259375f
Namya Aankur Gupta <65950853+Namyagupta@users.noreply.github.com>
2022-10-05 23:05:24 +0530

    Create managers_with_5_direct_reports.sql

---
+++ managers_with_5_direct_reports.sql
@@ -0,0 +1,24 @@
+/*Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write an SQL query to report the managers with at least five direct reports.
+*/
+
+SELECT a.name
+FROM Employee a JOIN Employee b
+ON a.id = b.managerId
+GROUP BY a.name
+HAVING COUNT(b.managerId)>=5
