2179dd1bedc4348983761981191cb4c35cd7e232
Namya Aankur Gupta <65950853+Namyagupta@users.noreply.github.com>
2022-10-07 20:28:10 +0530

    Create average_salary.sql

---
+++ average_salary.sql
@@ -0,0 +1,33 @@
+/*
+Table: Salary
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| employee_id | int  |
+| amount      | int  |
+| pay_date    | date |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table indicates the salary of an employee in one month.
+employee_id is a foreign key from the Employee table.
+ 
+
+Table: Employee
+
++---------------+------+
+| Column Name   | Type |
++---------------+------+
+| employee_id   | int  |
+| department_id | int  |
++---------------+------+
+employee_id is the primary key column for this table.
+Each row of this table indicates the department of an employee.
+ 
+
+Write an SQL query to report the comparison result (higher/lower/same) of the average salary of employees in a department to the company's average salary.
+
+Return the result table in any order.
+*/
+
